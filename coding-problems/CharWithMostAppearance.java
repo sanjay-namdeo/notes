faf409fa095ec9543ec2bdaafafcc732363e7f5c
Sanjay Namdeo <sanjay.namdeo@live.com>
2022-05-17 03:48:43 +0000

    Update CharWithMostAppearance

--- coding-problems/CharWithMostAppearance.java
+++ coding-problems/CharWithMostAppearance.java
@@ -2,10 +2,25 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.OptionalInt;
+import java.util.stream.Collectors;
 
 public class CharWithMostAppearance {
     public static void main(String[] args) {
+        String input = "Sanjay";
+        // By conventional methods
         System.out.println(charWithMostAppearance(""));
+
+        // By java 8
+        System.out.println(input
+                .chars()
+                .filter(c -> Character.isWhitespace(c) == false)
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey));
+        ;
     }
 
     private static Character charWithMostAppearance(String string) {
@@ -34,4 +49,5 @@ public class CharWithMostAppearance {
         System.out.println(charMap);
         return charMap;
     }
+
 }
