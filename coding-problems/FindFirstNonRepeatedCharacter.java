af02bed4b6843c0c73a97f3f9aa236353c1b997c
Sanjay Namdeo <sanjay.namdeo@live.com>
2022-05-17 09:28:07 +0000

    Update FindFirstNonRepeatedCharacter

--- coding-problems/FindFirstNonRepeatedCharacter.java
+++ coding-problems/FindFirstNonRepeatedCharacter.java
@@ -1,16 +1,31 @@
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class FindFirstNonRepeatedCharacter {
     public static void main(String[] args) {
-        Map<Character, Integer> charCount = getCharCount("Sanjay");
+        String input = "SanjayS";
+
+        // Traditional way
+        Map<Character, Integer> charCount = getCharCount(input);
         for (Entry<Character, Integer> entry : charCount.entrySet()) {
             if (entry.getValue() == 1) {
                 System.out.println(entry.getKey());
                 break;
             }
         }
+
+        // Java 8 way
+        System.out.println(input
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, LinkedHashMap::new, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue() == 1)
+                .map(Entry::getKey).findFirst());
     }
 
     private static Map<Character, Integer> getCharCount(String input) {
